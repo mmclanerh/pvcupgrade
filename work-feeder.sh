d245638b37710672d1327b40a35093dd425f2ffd
Mike McLane <mmclane@redhat.com>
2018-06-02 22:54:50 -0700

    updates

--- work-feeder.sh
+++ work-feeder.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-time for line in $(cat ebs-pvs.txt); do
-  oc -n ${project} get pvc --no-headers 2>&1| awk -vp="${project}" '{print p,$0}'
+time for project in $(awk '{print $1}' ebs-pvcs.txt); do
+  vol=$(oc -n ${project} get pvc --no-headers | awk '($6=="ebs") {print $1}'|head -n1)
+  echo "## ${project}:pvc/${vol}"
+  if [ -z "${vol}" ]; then
+    echo "skipping, ebs vol not found"
+    continue
+  fi
+  ./pvcmove ${project} ${vol} gluster-subvol
 done
